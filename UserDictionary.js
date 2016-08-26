d4d2dc5a4bf3a3c35294331063ec9ac6ed8e87d6
modoer <mikeumea@hotmail.com>
2016-08-26 13:52:03 +0200

    Create UserDictionary.js

---
+++ UserDictionary.js
@@ -0,0 +1,10 @@
+
+function refreshUserDict()
+{
+   dictionary["_dt"] = "ToDate";
+   dictionary["_df"] = "FromDate";
+   dictionary["ii"] = "INSERT INTO";
+   dictionary["va"] = "VALUES";
+   dictionary["ob"] = "ORDER BY";
+   
+};
